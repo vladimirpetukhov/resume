349c9195a2f149fdfed886ccd33a106b060fd2cf
vpetukhov <vladimir.petukhov@elando.com>
2021-12-03 17:26:29 +0200

    Updates

---
+++ precache-manifest.edadb54596e62422ea9d0b9a785837d4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6da2e97f5f6a5282c10525b855536a9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8c7bf663b8b76d3374ae",
+    "url": "/resume/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "3c7e3dd122b6a940ff2d",
+    "url": "/resume/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "1450dfcc50ef5efab156",
+    "url": "/resume/static/css/main.cc42584a.chunk.css"
+  },
+  {
+    "revision": "e16e7e83e71309c5f883",
+    "url": "/resume/static/js/2.db74f5b8.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/resume/static/js/2.db74f5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7bf663b8b76d3374ae",
+    "url": "/resume/static/js/3.ac1c6d58.chunk.js"
+  },
+  {
+    "revision": "3c7e3dd122b6a940ff2d",
+    "url": "/resume/static/js/4.6b6c2280.chunk.js"
+  },
+  {
+    "revision": "1450dfcc50ef5efab156",
+    "url": "/resume/static/js/main.76c7b2fb.chunk.js"
+  },
+  {
+    "revision": "a62404b6a52c11d28292",
+    "url": "/resume/static/js/runtime-main.05a1b60b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/resume/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "92f61122af60e60d09f46ba8f7625dce",
+    "url": "/resume/static/media/Bisoft_New.92f61122.png"
+  },
+  {
+    "revision": "76bc86584956928e20501ba0daacd226",
+    "url": "/resume/static/media/GISExplorerLogo_200x65.76bc8658.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/resume/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9735ad8959be83a8b8c1db58b17279fa",
+    "url": "/resume/static/media/Semansys.9735ad89.png"
+  },
+  {
+    "revision": "b59b9d6466e46e8827962624475d7901",
+    "url": "/resume/static/media/allianz.b59b9d64.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/resume/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/resume/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/resume/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/resume/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/resume/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "86ce3f1fc0002f860cb45e72a28558c6",
+    "url": "/resume/static/media/logo_infocape300.86ce3f1f.png"
+  },
+  {
+    "revision": "e04dbae1b5e7a90e57707991c61f5b10",
+    "url": "/resume/static/media/maaltijdDeal.e04dbae1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/resume/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/resume/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/resume/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/resume/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/resume/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53dff9707a54a1e88f9d0fdcc56937fd.js"
+  "/resume/precache-manifest.edadb54596e62422ea9d0b9a785837d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
